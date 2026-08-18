439b9844d57b30886fba1bdca7c095ff7c72a5fd
disocodes <41159841+disocodes@users.noreply.github.com>
2026-08-18 09:59:43 +0800

    Make NVIDIA bridge importable

---
+++ nvidia_bridge/__init__.py
@@ -0,0 +1,4 @@
+from .nvidia_push_adapter import DatabricksOptimizationBridge
+from .analysis_writer import NvidiaAnalysisWriter
+
+__all__ = ["DatabricksOptimizationBridge", "NvidiaAnalysisWriter"]
